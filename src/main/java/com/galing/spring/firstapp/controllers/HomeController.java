1b532b1564b8640403db721d819cd4c4db9332e9
Jorge Galindo Peña <jorgape225@gmail.com>
2020-09-29 14:02:42 +0200

    Created a custom properties file for storing literals, constants..

--- src/main/java/com/galing/spring/firstapp/controllers/HomeController.java
+++ src/main/java/com/galing/spring/firstapp/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.galing.spring.firstapp.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/app")
 public class HomeController {
 
+    @Value("${homecontroller.home.title}")
+    private String homeTitle;
+
     @RequestMapping(value = {"", "/", "/home", "/index"}, method = RequestMethod.GET)
     public String home(Model model){
-        model.addAttribute("title", "Hello World, Spring!");
+        model.addAttribute("title", homeTitle);
         return "home";
     }
 }
